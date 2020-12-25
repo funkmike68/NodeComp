275ac7f650869be7daa574673573bfd2cc325222
Funk Mike 68 <74109243+funkmike68@users.noreply.github.com>
2020-12-25 11:40:53 -0600

    Node: understanding requests and sending responses
    Node: understanding requests and sending responses

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 //arrow function
-const server = http.createServer((res, req) => {
-    console.log(req);
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>We are now live on my Node.js server!</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 server.listen(3000);
