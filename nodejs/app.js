4234074887fdb740de27ec35a877c158e0bf1353
Funk Mike 68 <74109243+funkmike68@users.noreply.github.com>
2020-12-26 18:06:45 -0600

    node routing requests
    node routing requests

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,14 +2,21 @@ const http = require('http');
 
 //arrow function
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter A Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>We are now live on my Node.js server!</h1></body>');
+    res.write('<body><h2>Hola from my Node.js server!</h2></body>');
     res.write('</html>');
     res.end();
+    
 });
 
 server.listen(3000);
