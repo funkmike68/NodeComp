80559ad38c956502bc1b06147c4fb818085eca84
Funk Mike 68 <74109243+funkmike68@users.noreply.github.com>
2020-12-26 18:28:08 -0600

    node redirecting requests
    node redirecting requests

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,13 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
 //arrow function
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter A Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
+        return res.end();
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY TEXT');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
     res.setHeader('Content-Type', 'text/html');
