2af99c1e927ecb928c627fe580b53859b88c7169
Funk Mike 68 <74109243+funkmike68@users.noreply.github.com>
2021-01-16 19:58:50 -0600

    2ndapp update - registering the routes
    2ndapp update - registering the routes

--- 2ndapp/controllers/admin.js
+++ 2ndapp/controllers/admin.js
@@ -16,17 +16,12 @@ exports.getAddProduct = (req, res, next) => {
     res.redirect('/');
 };
 
-exports.getProducts = (req, res, next) => {
+  exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
-        res.render('shop/product-list', {
-            prods: products, 
-            pageTitle: 'Shop',
-            path: '/', 
-            hasProducts: products.length > 0,
-            activeShop: true,
-            productCSS: true
-        });
+      res.render('admin/products', {
+          prods: products, 
+          pageTitle: 'Admin Products',
+          path: '/admin/products'
+      });
     });
-    
 };
-
