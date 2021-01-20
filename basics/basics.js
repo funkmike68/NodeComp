0128be40d485c93eaf17108487aa68aff7767db4
Funk Mike 68 <74109243+funkmike68@users.noreply.github.com>
2021-01-20 09:46:43 -0600

    javascript basics - arrow functions

--- basics/basics.js
+++ basics/basics.js
@@ -15,3 +15,25 @@ let isMarried = true;
 let age = 52;
 console.log(`My name is ${firstName} ${lastName}. I live in ${city}, ${state}.`);
 console.log(`${firstName} is a ${age} year old ${job}. He is married. ${isMarried}`);
+
+// Arrow functions
+const multiply = (a, b) => {
+    return a * b;
+}
+
+console.log(multiply(10, 12));
+
+// shorter version of an arrow function
+const add = (x, y) => x + y;
+console.log(add(16, 34));
+
+
+const subTwo = c => c - 2;
+console.log(subTwo(12));
+
+// arrow function with no arguement
+const addRandom = () => 12 + 24;
+console.log(addRandom());
+
+
+
