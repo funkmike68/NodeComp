0f9b45b906407652ac8b1d77927529e53357e9fb
Michael Dodson <miked@dominicansoul.com>
2021-04-11 03:10:33 -0500

    Add a .gitignore file
    Add a .gitignore file

--- dragonball/app.js
+++ dragonball/app.js
@@ -14,11 +14,11 @@ const formsRoutes = require('./routes/forms');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes); 
+app.use('/admin', adminRoutes);
 app.use(formsRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found', path: 'Error'}); 
+    res.status(404).render('404', {pageTitle: 'Page Not Found', path: 'Error'});
 });
 
 app.listen(3000);
